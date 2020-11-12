d27429434b36b806afe9c95ef024cccbb98c9d43
GPAD-DES-07\GPAD <irfanhossain2025@gmail.com>
2020-11-12 18:12:22 +0600

    hello solve

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // reset the users table
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+        // generate  3  users table 
+        DB:table('users')->insert([
+            [
+                'name' => " jhon Doe",
+                'email' => "johdfa@gmail.com",
+                'password' => bcrypt('secret')
+
+            ],
+
+            [
+                'name' => "Main Doe",
+                'email' => "Amin@gmail.com",
+                'password' => bcrypt('secret')
+
+            ],
+            [
+                'name' => " himu Doe",
+                'email' => "himu@gmail.com",
+                'password' => bcrypt('secret')
+
+            ],
+        ]);
+    }
+}
